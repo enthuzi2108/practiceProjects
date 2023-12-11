039ba8c90a406bb665cb05507757a8681c132a44
enthuzi2108 <141907704+enthuzi2108@users.noreply.github.com>
2023-12-11 21:44:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import WeatherApp from './components/Weather App/weatherApp';
+
+function App() {
+  return (
+    <div className="App">
+<WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
